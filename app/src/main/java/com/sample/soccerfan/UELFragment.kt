5d8f7bb6b68b1d6b8e44e84369a828e140c7bb69
Andrada Anca <>
2022-05-25 10:02:31 +0300

    Applies styling

--- app/src/main/java/com/sample/soccerfan/UELFragment.kt
+++ app/src/main/java/com/sample/soccerfan/UELFragment.kt
@@ -1,6 +1,7 @@
 package com.sample.soccerfan
 
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,8 @@ import android.view.ViewGroup
 class UELFragment: BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_uel, container, false)
+        val contextThemeWrapper = ContextThemeWrapper(requireActivity(), R.style.Theme_SoccerFan_UEL)
+        val themedInflater = inflater.cloneInContext(contextThemeWrapper)
+        return themedInflater.inflate(R.layout.fragment_uel, container, false)
     }
 }
